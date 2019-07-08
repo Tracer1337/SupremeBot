3e4dab664e9c74c93df3ca4f2951ad117bee88b2
Merlin Moelter <joebau.0815@googlemail.com>
2019-07-08 23:26:14 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// https://www.supremenewyork.com/shop/all/accessories
+const products = document.getElementsByTagName("article")
+const result = []
+Array.from(products).forEach(e => {
+	const $ = s => e.querySelector(".inner-article "+s).innerText
+	const name = $("h1")
+	const color = $("p")
+	result.push({name, color})
+})
+console.table(result)
+
+const test = (element, selector, query) => element.querySelector(".inner-article "+selector).innerText.toLowerCase().includes(query.toLowerCase())
+
+const getProductByNameAndColor = (name, color) => Array.from(products).find(e => test(e, "h1", name) && test(e, "p", color))
+
+const getProductLink = article => article.querySelector(".inner-article a").href
