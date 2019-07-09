6a55b0b9410506d628d5b2e8308c8ee870fa8af5
Merlin Moelter <joebau.0815@googlemail.com>
2019-07-09 21:16:47 +0200

    readme

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
 # Supreme Bot
-&copy; 2019 Merlin Moelter
+* [System Requirements](#anchor-requirements)
+* [Configuration](#anchor-configuration)
+* [Usage](#anchor-usage)
 
-## Configuration
+<a name="anchor-requirements"></a>
+# System Requirements
+In order to run the bot, you have to have the latest version of [Node.js](https://nodejs.org/en/download/) installed.
+It is recommended to have a fast internet connection, since it is all about speed.
+
+<a name="anchor-configuration"></a>
+# Configuration
+There are two files you have to change for the bot to work proberly.
+1. [products.json](#anchor-configuration-products)
+1. [checkout.json](#anchor-configuration-checkout)
+<a name="anchor-configuration-config"></a>
+### products.json
+This file stores data about the products you want to order. The bot will try to order everything listed in this file with the specifications defined. The scheme is:
+```JSON
+[
+  {
+    "name": "A keyword that will be contained in the products name (e.g. the name is 'Supreme®/Nike®/NBA Headband' your keyword could be 'Headband')",
+
+    "category": "The category the product will be in (e.g. accessories, tshirts, hats, ...)",
+
+    "color": "The color which you want the product to be (e.g. red, black, yellow, ...)",
+
+    "size": "This argument is optional, that means the bot will take the smallest available size if nothing is specified here. (e.g. small, medium, large, ...)"
+  }
+]
+```
+The file is already filled with some data, so you only have to change the fields existing or delete some.
+
+<a name="anchor-configuration-checkout"></a>
+### checkout.json
+This file contains the checkout details which will be auto-filled by the bot. This file does not exist already, but there is a template file with blank fields which you have to fill out with you personal data. It is pretty much self explaining.
+
+<a name="anchor-usage"></a>
+# Usage
+Before you can use the bot, you have to run ``npm install`` inside the root directory of the bot. It will install all dependencies needed and with them the chromium browser, that's why it may take a while to run this command. This command is only neccessary once.
+
+To start the bot you have to run ``node index.js`` in the terminal inside the root folder of the bot.
+
+Once the bot is running it will create an instance of chromium in which it will excecute the order. The bot will finish on the checkout page on which you only have to accept the order. 
+
+&copy; 2019 Merlin Moelter
--- index.js
+++ index.js
@@ -1,18 +1,74 @@
-// https://www.supremenewyork.com/shop/all/accessories
-const products = document.getElementsByTagName("article")
-const result = []
-Array.from(products).forEach(e => {
-	const $ = s => e.querySelector(".inner-article "+s).innerText
-	const name = $("h1")
-	const color = $("p")
-	result.push({name, color})
-})
-console.table(result)
+const puppeteer = require("puppeteer")
+const chalk = require("chalk")
+const config = require("./config.json")
+const checkout_autofill = require("./checkout.json")
+const productsToOrder = require("./products.json")
 
+// Sleep t seconds
+const sleep = t => new Promise(resolve => setTimeout(resolve, t*1000))
 
-const getProductByNameAndColor = (name, color) => {
-	const test = (element, selector, query) => element.querySelector(".inner-article "+selector).innerText.toLowerCase().includes(query.toLowerCase())
-	Array.from(products).find(e => test(e, "h1", name) && test(e, "p", color))
+// Add product to cart
+const addToCart = async (page, product) => {
+  await page.goto(config.pages.shop+product.category)
+
+  // Get URL of product
+  const url = await page.evaluate((product, selectors) => {
+    const products = document.getElementsByTagName(selectors.product)
+    const test = (element, selector, query) => element.querySelector(selectors.product_inner+" "+selector).innerText.toLowerCase().includes(query.toLowerCase())
+    const getProductByNameAndColor = (name, color) => Array.from(products).find(e => test(e, selectors.product_name, name) && test(e, selectors.product_color, color))
+    const getProductLink = article => article.querySelector(selectors.product_inner+" "+selectors.product_link).href
+    const isSoldOut = article => !!article.querySelector(selectors.product_sold_out)
+
+    productDOMElement = getProductByNameAndColor(product.name, product.color)
+
+    if(isSoldOut(productDOMElement))
+      return Promise.resolve(false)
+
+    return Promise.resolve(getProductLink(productDOMElement))
+  }, product, config.selectors)
+
+  if(!url) return Promise.resolve(false)
+
+  await page.goto(url)
+
+  // Set size if neccessary
+  if(product.size) await page.evaluate(size => {
+    const selectElement = document.getElementById("size")
+    const match = Array.from(selectElement.getElementsByTagName("option")).filter(e => e.innerText.toLowerCase().includes(size.toLowerCase()))[0]
+    if(match)
+      selectElement.value = match.value
+  }, product.size)
+
+  // Click on "add to cart"
+  await page.click(config.selectors.add_to_cart)
+  return Promise.resolve(true)
 }
 
-const getProductLink = article => article.querySelector(".inner-article a").href
+// Main function
+const run = async () => {
+  const browser = await puppeteer.launch({headless: false, args: ["--start-maximized"]})
+  const page = await browser.newPage()
+  await page.setViewport({width: 1920, height: 800})
+  // Setting Cookies specified in config.json
+  await page.goto(config.pages.main)
+  config.cookies.forEach(async cookie => await page.setCookie({name: cookie.name, value: cookie.value}))
+
+  // Add all products to cart
+  console.log("Trying to order:")
+  console.table(productsToOrder)
+  for(let p of productsToOrder){
+    const addedToCart = await addToCart(page, p)
+    if(!addedToCart) console.log(chalk.red(`${p.name} - ${p.color}: sold out`))
+    else console.log(chalk.green(`${p.name} - ${p.color}: added to cart`))
+    await sleep(.1)
+  }
+
+  // Checkout
+  await page.goto(config.pages.checkout)
+  await page.evaluate((data) => {
+    for(let field of data)
+      document.getElementsByName(field.name)[0].value = field.value
+  }, checkout_autofill)
+  page.$(config.selectors.checkout_terms).then(async e => await e.click())
+}
+run()
